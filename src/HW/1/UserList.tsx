1d3d8a31c12fa6075417985aa99c1c2079fba68c
ottolol <alexsveshnik@gmail.com>
2025-02-02 12:47:16 +0300

    done HW01

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -4,26 +4,34 @@ type AddressType = {
 };
 
 type UserType = {
-  // ПРИДЕТСЯ САМОМУ)
-  users:
+  // ПРИДЕТСЯ САМОМУ
+  id: number;
+  name: string;
+  age: number;
+  address: AddressType;
 };
 
 type UserListPropsType = {
-  users: UserType; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+  users: UserType[]; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
 };
 
-export const UserList = (props: 'ВСТАВЛЯЕМ ТИПИЗАЦИЮ') => {
+export const UserList = (props: UserListPropsType) => {
   return (
-    <div id={'hw01-users'}>
+    <div id={"hw01-users"}>
       <h2>User List:</h2>
 
       <ul>
-        {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
-          <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>
-            <strong>{ЭЛЕМЕНТ МАССИВА.name}</strong> (Age: {ЭЛЕМЕНТ МАССИВА.age})<strong> Address:</strong>
-            {ЭЛЕМЕНТ МАССИВА.address.street}, {ЭЛЕМЕНТ МАССИВА.address.city}
-          </li>
-        ))}
+        {props.users.map(
+          (
+            user // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+          ) => (
+            <li key={user.id} id={`hw01-user-${user.id}`}>
+              <strong>{user.name}</strong> (Age: {user.age})
+              <strong> Address:</strong>
+              {user.address.street}, {user.address.city}
+            </li>
+          )
+        )}
       </ul>
     </div>
   );
